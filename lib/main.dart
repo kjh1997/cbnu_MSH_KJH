b978ff10cd4ef3cc83625e01e7ce3620ed82033e
kjh1997 <aam42@naver.com>
2021-12-30 14:46:22 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,45 @@
 import 'package:flutter/material.dart';
+import 'imageWidget.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  var switchValue = false;
-  String test = 'hello';
-  Color _color = Colors.black;
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primaryColor: Colors.blueAccent,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
+      title: 'MaterialApp',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ImageWidgetApp(),
+    );
+  }
+}
+
+class MaterialFlutterApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MaterialFlutterApp();
+  }
+}
+
+class _MaterialFlutterApp extends State<MaterialFlutterApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Material desgin app"),),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.android),
+              Text('android')
+            ],
+          ),
         ),
-        darkTheme: ThemeData.light(),
-        home: Scaffold(
-          appBar: AppBar(title: Text("appbar")),
-          bottomNavigationBar: BottomAppBar(
-            child: Row(
-              children: [
-                IconButton(
-                    onPressed: () {}, icon: Icon(Icons.icecream_outlined)),
-                IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
-                IconButton(onPressed: () {}, icon: Icon(Icons.access_alarm))
-              ],
-            ),
-          ),
-          body: Center(
-            child: ElevatedButton(child: Text('$test'),
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(_color)
-            ),
-            onPressed: () {
-              if (test == 'hello'){
-                setState(() {
-                  test = 'flutter';
-                  _color = Colors.amber;
-                });
-
-              } else {
-                setState(() {
-                  test= 'hello';
-                  _color = Colors.blue;
-                });
-              }
-
-            }),
-          ),
-        ));
+      ),
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.add),
+      onPressed: (){
+        print('버튼클릭');
+      },),
+    );
   }
 }
