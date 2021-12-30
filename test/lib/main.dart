a7efbed77b2f03fac938636ceb3fc06179057592
kjh1997 <aam41@naver.com>
2021-12-30 20:13:19 +0900

    12302013

--- test/lib/main.dart
+++ test/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'sub/firstpage.dart';
+import 'sub/secondpage.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -22,92 +25,42 @@ class WidgetApp extends StatefulWidget {
   _WidgetExampleState createState() => _WidgetExampleState();
 }
 
-class _WidgetExampleState extends State<WidgetApp> {
-  String sum = '';
-  TextEditingController value1 = TextEditingController();
-  TextEditingController value2 = TextEditingController();
-  List _buttonList = ['더하기', '빼기', '곱하기', '나누기'];
-  List<DropdownMenuItem<String>> _dropDownMenuItems = new List.empty(growable: true);
-  String? buttonText;
+class _WidgetExampleState extends State<WidgetApp>
+    with SingleTickerProviderStateMixin {
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
+  }
+
+  TabController? controller;
 
   @override
   void initState() {
     super.initState();
-    for (var item in _buttonList) {
-      _dropDownMenuItems.add(DropdownMenuItem(value: item, child: Text(item)));
-    }
-    buttonText = _dropDownMenuItems[0].value;
+    controller = TabController(length: 2, vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Widget test"),
+        title: Text('TabBar Example'),
       ),
-      body: Container(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-
-              Padding(
-                padding: const EdgeInsets.all(15),
-                child: Text('결과 : $sum', style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                    keyboardType: TextInputType.number, controller: value2),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  controller: value1,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(15),
-                child: ElevatedButton(
-                    child: Row(
-                      children: <Widget>[Icon(Icons.add), Text(buttonText!)],
-                    ),
-                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all( Colors.amber)),
-                    onPressed: () {
-                      setState(() {
-                        var value1Int = double.parse(value1.value.text);
-                        var value2Int = double.parse(value2.value.text);
-                        var result;
-                        if (buttonText == '더하기') {
-                          result = value1Int + value2Int;
-                        } else if (buttonText == '빼기') {
-                          result = value1Int - value2Int;
-                        } else if (buttonText == '곱하기') {
-                          result = value1Int * value2Int;
-                        } else {
-                          result = value1Int / value2Int;
-                        }
-                        sum = result.toString();
-                      });
-                    }),
-              ),
-              Padding(
-                padding: EdgeInsets.all(15),
-                child: DropdownButton(
-                  items: _dropDownMenuItems,
-                  onChanged: (String? value) {
-                    setState(() {
-                      buttonText = value;
-                    });
-                  },
-                  value: buttonText,
-                ),
-              )
-            ],
+      body: TabBarView(
+        children: <Widget>[FirstApp(), SecondApp()],
+        controller: controller,
+      ),
+      bottomNavigationBar: TabBar(
+        tabs: <Tab>[
+          Tab(
+            icon: Icon(Icons.looks_one, color: Colors.blueAccent),
           ),
-        ),
+          Tab(
+            icon: Icon(Icons.looks_two, color: Colors.blueAccent),
+          )
+        ],controller: controller,
       ),
     );
-  }
+  }00
 }
