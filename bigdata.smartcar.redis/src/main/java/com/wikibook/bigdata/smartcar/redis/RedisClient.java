c5be4a800686bfdafc06dc145a3370751f155480
kjh1997 <aam42@naver.com>
2022-02-02 21:46:54 +0900

    redis and firebase linkage

--- bigdata.smartcar.redis/src/main/java/com/wikibook/bigdata/smartcar/redis/RedisClient.java
+++ bigdata.smartcar.redis/src/main/java/com/wikibook/bigdata/smartcar/redis/RedisClient.java
@@ -1,5 +1,18 @@
 package com.wikibook.bigdata.smartcar.redis;
 
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
+
+
 import java.util.Set;
 
 import redis.clients.jedis.Jedis;
@@ -7,12 +20,18 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 
+@SuppressWarnings("unused")
 public class RedisClient extends Thread{
+	public static FirebaseOptions option;
+    public static Firestore db; 
+    public final static String PATH = "./kjh-prj-firebase-adminsdk-4ufoa-e7df9f3dda.json";
+    public final static String COLLECTION_NAME = "컬렉션";
 
 	private String key;
 	private Jedis jedis;
 
 
+	@SuppressWarnings("resource")
 	public RedisClient(String k) {
 
 		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
@@ -26,7 +45,15 @@ public class RedisClient extends Thread{
 
 	@Override    
 	public void run() {
-		
+        
+		try {
+			init();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        makeDatabaseConn();
+        select();		
 
 		Set<String> overSpeedCarList = null;
 		
@@ -44,10 +71,10 @@ public class RedisClient extends Thread{
 				
 				
 				System.out.println("\n[ Try No." + cnt++ + "]");
-
 				if(overSpeedCarList.size() > 0) {
 					for (String list : overSpeedCarList) {
 						System.out.println(list);
+						insert(list);
 					}
 					System.out.println("");
 					
@@ -70,4 +97,30 @@ public class RedisClient extends Thread{
 			if( jedis != null ) jedis.close();
 		}
 	}
+	public static void init() throws Exception{
+        FileInputStream refreshToken = new FileInputStream(PATH);
+        option = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                .setDatabaseUrl("https://kjh-prj-default-rtdb.firebaseio.com")
+                .build();
+        FirebaseApp.initializeApp(option);
+    }
+    
+    
+    static void makeDatabaseConn(){  //Firestore 인스턴스 생성
+        db = FirestoreClient.getFirestore();
+    }    
+    
+    static void select(){ //조회
+        db.collection(COLLECTION_NAME).addSnapshotListener( (target, exception)->{
+            System.out.println(" - select start - ");
+            System.out.println(" - select end - ");
+        });
+    }    
+   
+    static void insert(String a){  //등록
+        Map<Object, Object> item = new HashMap<Object, Object>();
+        item.put("test", a);
+        db.collection(COLLECTION_NAME).add(item);
+    }
 }
